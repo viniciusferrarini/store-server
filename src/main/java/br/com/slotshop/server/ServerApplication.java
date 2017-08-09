50447e6436635648d796b39c623a16b9068b6bfe
Vinícius Ferrarini <vinicius.ferrarini@viasoft.com.br>
2017-08-09 09:01:30 -0300

    - configurações oauth2

--- src/main/java/br/com/slotshop/server/ServerApplication.java
+++ src/main/java/br/com/slotshop/server/ServerApplication.java
@@ -2,8 +2,10 @@ package br.com.slotshop.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
